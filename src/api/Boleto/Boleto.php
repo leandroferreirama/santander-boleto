626206af9f8a758205016ea6e151747c56b74b8e
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2024-06-17 08:50:28 -0300

    Inclusão do DV no nosso número

--- src/api/Boleto/Boleto.php
+++ src/api/Boleto/Boleto.php
@@ -24,18 +24,63 @@ class Boleto implements \JsonSerializable
     public string $paymentType;
     public Payer $payer;
     public Key $key;
+    public int $bankNumber;
 
     public function __construct(
-        public string $nsuCode, public string $environment, public string $covenantCode, public int $bankNumber, public string $clientNumber, 
+        public string $nsuCode, public string $environment, public string $covenantCode, $bankNumber, public string $clientNumber, 
         public string $dueDate, public string $nominalValue, public string $documentKind, $documentType, $documentNumber, $name, $address, $neighborhood, $city, 
         $state, $zipCode, $type, $dictKey, public string $finePercentage = '0.00', public int $fineQuantityDays = 0, public string $interestPercentage = '0.00'
     )
     {
+        $this->bankNumber = $this->bankNumberDv($bankNumber);
         $this->nsuDate = date("Y-m-d");
         $this->issueDate = date("Y-m-d");
         $this->paymentType = 'REGISTRO';
         $this->payer = new Payer($documentType, $documentNumber, $name, $address, $neighborhood, $city, $state, $zipCode);
         $this->key = new Key($type, $dictKey);
     }
+
+    private function modulo11($num, $base=9, $r=0)
+    {
+        $soma = 0;
+        $fator = 2;
+
+        /* Separacao dos numeros */
+        for ($i = strlen($num); $i > 0; $i--) {
+            // pega cada numero isoladamente
+            $numeros[$i] = substr($num,$i-1,1);
+            // Efetua multiplicacao do numero pelo falor
+            $parcial[$i] = $numeros[$i] * $fator;
+            // Soma dos digitos
+            $soma += $parcial[$i];
+            if ($fator == $base) {
+                // restaura fator de multiplicacao para 2 
+                $fator = 1;
+            }
+            $fator++;
+        }
+
+        /* Calculo do modulo 11 */
+        if ($r == 0) {
+            $soma *= 10;
+            $digito = $soma % 11;
+            if ($digito == 10) {
+                $digito = 0;
+            }
+            return $digito;
+        } elseif ($r == 1){
+            $resto = $soma % 11;
+            return $resto;
+        }
+    }
     
+    private function bankNumberDv($bankNumber)
+    {
+        $resto = $this->modulo11($bankNumber, 9, 1);
+        $digito = 11 - $resto;
+        if($digito == 10 || $digito == 11){
+            $digito = 0;
+        }
+        return $bankNumber.$digito;
+    }
 }
